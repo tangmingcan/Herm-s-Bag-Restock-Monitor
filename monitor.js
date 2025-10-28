4641d557d3064a14f0e60c77927b9f69a1e8ff92
tangmingcan <31913209+tangmingcan@users.noreply.github.com>
2025-10-28 14:05:57 +0000

    Update monitor.js
    Test one time refresh

--- monitor.js
+++ monitor.js
@@ -1,24 +1,25 @@
-var producturl = 'https://www.hermes.com/de/de/product/mini-tasche-verrou-strap-H083248CKAA/";
+var producturl = "https://www.hermes.com/uk/en/product/2424-21-bag-H082153CKAI/";
 var audio = new Audio("https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3");
 var doSearch = function(){
-  setTimeout(() => {
+  //alert('1');
+  //setTimeout(() => {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
+      //console.log(this.responseText);
       if (this.readyState == 4 && this.status == 200 && this.responseText){
-        if (this.responseText.includes('add-to-cart-button-in-stock')){
-          console.log(['Available Now', new Date()]);
-          audio.play();
+        if (this.responseText.includes('Unfortunately this product is no longer')) {
+            console.log(['Not Available', new Date()]);
         }
         else{
-          console.log('Sold Out');
+          console.log('Yes');
         }
       }
 
     };
     xhttp.open('GET',producturl, true);
     xhttp.send()
-    doSearch();
-  }, 90000 + Math.floor(Math.random()*60000));
+  //  doSearch();
+  //}, 90000 + Math.floor(Math.random()*60000));
 }
 // initial call
 doSearch();
